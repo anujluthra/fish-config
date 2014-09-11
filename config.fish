36f1b271751057e26e92d52b602cd06f825a3d31
anuj luthra <anuj.luthra@redbubble.com>
2014-09-12 09:57:26 +1000

    adding initial fish configs

---
+++ config.fish
@@ -0,0 +1,22 @@
+alias gup="git pull --rebase"
+alias gcm="git checkout master"
+
+alias rb='cd ~/projects/redbubble'
+alias efe='rb and bundle exec rake workers:export_queue FILE=~/errors.json QUEUE=fulfillment_workers_errors --trace'
+alias ete='rb and bundle exec rake workers:export_queue FILE=~/errors.json QUEUE=transfer_workers_errors'
+alias ebe='rb and bundle exec rake workers:export_queue FILE=~/errors.json QUEUE=background_workers_errors'
+alias ie='rb and bundle exec rake workers:import_queue FILE=~/errors.json'
+alias rme='rm -v ~/errors.json'
+alias lq='sudo rabbitmqctl list_queues'
+alias lqe='sudo rabbitmqctl list_queues | grep errors'
+
+alias be='bundle exec'
+alias civnc='open vnc://jenkins.local:5010'
+alias migrate='be rake db:migrate parallel:prepare[8]; RAILS_ENV=cucumber rake db:migrate'
+
+alias gcm='git checkout master'
+alias gu='git up'
+alias gco='git checkout -b'
+
+
+setenv EDITOR "subl -w"
